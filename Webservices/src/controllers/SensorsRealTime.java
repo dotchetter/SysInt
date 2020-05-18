1bc5c932e7c87d77b7a6fed45a972f91a304355a
dotchetter <dotchetter@protonmail.ch>
2020-05-18 13:58:05 +0200

    Added the StaticDeviceMessageQueue polling and EmbeddedDeviceHandle class to the web service. The objects are static, which enables multiple instances of the service to use the same objects for checking against with cache.

--- Webservices/src/controllers/SensorsRealTime.java
+++ Webservices/src/controllers/SensorsRealTime.java
@@ -1,8 +1,11 @@
 package controllers;
 import com.google.gson.Gson;
 import models.SensorLogEntry;
+import repositories.EmbeddedDeviceHandle;
 import repositories.SensorDAO;
 import repositories.SensorType;
+import repositories.StaticDeviceMessageQueue;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -19,6 +22,9 @@ public class SensorsRealTime
     Session session = null;
     SensorDAO sensorDAO = new SensorDAO();
     Timer timer = new Timer();
+    private static Thread threadForDeviceHandle = null;
+    private static EmbeddedDeviceHandle deviceHandle = new EmbeddedDeviceHandle("Stockholm");
+    private static SensorLogEntry latestEntry = null;
 
     public SensorsRealTime() throws IOException
     {
@@ -46,15 +52,34 @@ public class SensorsRealTime
         timer.scheduleAtFixedRate(timerTask, 0, 1000);
     }
 
-    public void sendData() throws IOException, SQLException
+    public synchronized void sendData() throws IOException, SQLException
     {
-        if (session != null)
-        {
+        if (deviceHandle == null) {
+            deviceHandle = new EmbeddedDeviceHandle("Stockholm");
+        }
+
+        if (threadForDeviceHandle == null) {
+            threadForDeviceHandle = new Thread(deviceHandle);
+            threadForDeviceHandle.start();
+        }
+
+        var entry = StaticDeviceMessageQueue.dequeue();
+
+        if (latestEntry == null) {
+            latestEntry = StaticDeviceMessageQueue.dequeue();
+            session.getBasicRemote().sendText(new Gson().toJson("No messages received from sensor since start."));
+        }
+
+        if (session != null && entry != null) {
             var timestamp = new Timestamp(Instant.now().toEpochMilli());
             List<SensorLogEntry> sensorList = new ArrayList<>();
-            sensorList.add(new SensorLogEntry(new Random().nextFloat()*100, timestamp, "Uppsala", SensorType.TEMPERATURE));
-            sensorList.add(new SensorLogEntry(new Random().nextFloat()*100, timestamp, "Uppsala", SensorType.HUMIDITY));
-            sensorList.add(new SensorLogEntry(new Random().nextFloat()*100, timestamp, "Uppsala", SensorType.LUMEN));
+            sensorList.add(latestEntry);
+            SensorLogEntry newLogEntry = StaticDeviceMessageQueue.dequeue();
+
+            if (newLogEntry != null) {
+                latestEntry = newLogEntry;
+            }
+
             session.getBasicRemote().sendText(new Gson().toJson(sensorList));
         }
     }
