11dd2d3a31098a58eb385ec18c3f57c36466f3c7
dotchetter <dotchetter@protonmail.ch>
2020-05-18 10:06:30 +0200

    Testing the EmbeddedDeviceHandle with StaticDeviceMessageQueue in simulated runtime loop.

--- Webservices/src/repositories/Main.java
+++ Webservices/src/repositories/Main.java
@@ -11,9 +11,26 @@ public class Main {
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException
     {
 	    // write your test code here
-        SensorDAO dao = new SensorDAO();
+        //SensorDAO dao = new SensorDAO();
+        EmbeddedDeviceHandle deviceHandle = new EmbeddedDeviceHandle("Stockholm");
+        Thread threadForDeviceHandle = new Thread(deviceHandle);
 
-        //Create log entry
+        threadForDeviceHandle.start();
+
+
+        for (;;) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            SensorLogEntry i = StaticDeviceMessageQueue.dequeue();
+            //System.out.println("Waiting");
+            if (i == null) {continue;}
+            System.out.println(i.getValue() + i.getCity() + i.getSensorType());
+        }
+
+/*        //Create log entry
         dao.createLogEntry(new SensorLogEntry(50, new Timestamp(Instant.now().toEpochMilli()), "Uppsala", SensorType.HUMIDITY));
 
         //Get log entries
@@ -23,6 +40,6 @@ public class Main {
             System.out.println(entry.getValue());
             System.out.println(entry.getCreated());
             System.out.println(entry.getCity());
-        }
+        }*/
     }
 }
