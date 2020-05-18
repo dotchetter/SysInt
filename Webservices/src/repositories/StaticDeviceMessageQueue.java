df0a57a5d03a5a56436b133e232657322d2efa7f
dotchetter <dotchetter@protonmail.ch>
2020-05-18 10:04:44 +0200

    Bug fix: Data Structure LinkedList used with the queue interface.

--- Webservices/src/repositories/StaticDeviceMessageQueue.java
+++ Webservices/src/repositories/StaticDeviceMessageQueue.java
@@ -1,22 +1,28 @@
 package repositories;
 
 import models.SensorLogEntry;
-
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class StaticDeviceMessageQueue {
+class StaticDeviceMessageQueue {
 
-    private static Queue<SensorLogEntry> logQueue;
+    private static Queue<SensorLogEntry> logQueue = new LinkedList<>();
 
     public static void enqueue(SensorLogEntry logEntry) {
         try {
             logQueue.add(logEntry);
         } catch (Exception e) {
+            System.err.println("Error could not enqueue: " + logEntry);
             e.printStackTrace();
         }
     }
 
     public static SensorLogEntry dequeue() {
         return logQueue.poll();
+    }
+}
+
+class testQueue {
+    public static void main(String[] args) {
     }
 }
