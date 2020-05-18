9cf7fea47b855ff8ae7d350a5a57401319e4c82b
Yousernaym <mickeh.se@hotmail.com>
2020-05-18 15:31:54 +0200

    Updated javascript to sort websocket messages on the web page after sensor type, and also included data from servlet
    Removed commented out code

--- Webservices/src/controllers/SensorsRealTime.java
+++ Webservices/src/controllers/SensorsRealTime.java
@@ -1,9 +1,6 @@
 package controllers;
 import com.google.gson.Gson;
-import models.SensorLog;
-import models.SensorLogEntry;
 import repositories.EmbeddedDeviceHandle;
-import repositories.SensorType;
 import repositories.StaticDeviceMessageQueue;
 
 import javax.websocket.OnClose;
@@ -12,8 +9,6 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.*;
 
 @ServerEndpoint("/SensorsRealTime")
@@ -23,7 +18,6 @@ public class SensorsRealTime
     Timer timer = new Timer();
     private static Thread threadForDeviceHandle = null;
     private static EmbeddedDeviceHandle deviceHandle = new EmbeddedDeviceHandle("Stockholm");
-    //private static SensorLog latestEntry = null;
 
     public SensorsRealTime()
     {
@@ -63,24 +57,8 @@ public class SensorsRealTime
         }
 
         var entry = StaticDeviceMessageQueue.dequeue();
-
-        //if (latestEntry == null) {
-          //  latestEntry = StaticDeviceMessageQueue.dequeue();
-            //session.getBasicRemote().sendText(new Gson().toJson("No messages received from sensor since start."));
-        //}
-
-        if (entry != null) {
-            //var timestamp = new Timestamp(Instant.now().toEpochMilli());
-            //SensorLog sensorList = new ArrayList<>();
-            //sensorList.add(latestEntry);
-            //SensorLog newLogEntry = StaticDeviceMessageQueue.dequeue();
-
-            //if (newLogEntry != null) {
-              //  latestEntry = newLogEntry;
-            //}
-
+        if (entry != null)
             session.getBasicRemote().sendText(new Gson().toJson(entry));
-        }
     }
 
     @OnClose
--- Webservices/web/index.jsp
+++ Webservices/web/index.jsp
@@ -3,17 +3,24 @@
     <title></title>
   </head>
   <body>
-  <div id="wsMsg"></div>
+  <h1>Realtidsdata</h1>
+  <div id="TEMPERATURE"></div>
+  <div id="HUMIDITY"></div>
+  <div id="LUMEN"></div>
+  <h1>Databas</h1>
+  <div id="database"</body>
+
   <script>
     let ws = new WebSocket("ws://localhost:8080/Webservices_war_exploded/SensorsRealTime");
     ws.onmessage = function(e)
     {
-      document.getElementById("wsMsg").innerHTML = e.data;
+      let data = JSON.parse(e.data);
+      document.getElementById(data.sensorType).innerHTML = e.data;
     }
 
-    // fetch("http://localhost:8080/Webservices_war_exploded/SensorsDb")
-    //         .then(response => response.text())
-    //         .then(data => document.getElementById("wsMsg").innerHTML = data);
+     fetch("http://localhost:8080/Webservices_war_exploded/SensorsDb")
+             .then(response => response.text())
+             .then(data => document.getElementById("database").innerHTML = data);
 
   </script>
   </body>
